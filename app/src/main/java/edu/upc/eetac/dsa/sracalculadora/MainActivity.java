04840c11e2a0813e2f0115bd225c233414ec837f
IfJosean <josean7jkami@gmail.com>
2017-11-30 21:09:56 +0100

    operations

--- app/src/main/java/edu/upc/eetac/dsa/sracalculadora/MainActivity.java
+++ app/src/main/java/edu/upc/eetac/dsa/sracalculadora/MainActivity.java
@@ -144,7 +144,47 @@ public class MainActivity extends AppCompatActivity {
         ope=4;
     }
 
+    public void igual (View v){
+        try{
+            String aux2=pant.getText().toString():
+            operan2=Double.parseDouble(aux2);
+        } catch (NumberFormatException nfe) {}
+        pant.setText("");
 
+
+        if (ope == 1) {
+            res=operan1+operan2;
+        }else if (ope==2){
+            res=operan1-operan2;
+        }else if (ope==3){
+            res=operan1*operan2;
+        }else if (ope==4){
+            if (operan2==0){
+                pant.setText("Error");
+            }
+            res=operan1/operan2;
+        }
+        pant.setText(""+res);
+        operan1=res;
+    }
+
+    public void clear (View v){
+        pant.setText("");
+        operan1=0.0;
+        operan2=0.0;
+        res=0.0;
+    }
+
+    public void borrar (View v){
+        if (!pant.getText().toString().equals("")){
+            pant.setText(pant.getText().subSequence(0, pant.getText().length()-1)+"");
+
+        }
+    }
+
+    public void off (View v){
+        finish()
+    }
 
 
 
